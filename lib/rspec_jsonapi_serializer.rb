bde7856a6b04fd8f1e953dd33a46bdbf53df1ff7
Mateus Cruz <makteus@gmail.com>
2021-04-10 01:58:24 -0300

    Cleanup bundler template

--- lib/rspec_jsonapi_serializer.rb
+++ lib/rspec_jsonapi_serializer.rb
@@ -4,6 +4,4 @@ require "rspec_jsonapi_serializer/version"
 require "rspec_jsonapi_serializer/matchers"
 
 module RSpecJSONAPISerializer
-  class Error < StandardError; end
-  # Your code goes here...
 end
