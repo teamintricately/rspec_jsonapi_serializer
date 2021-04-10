3134446d0a963ab4edb1e47e9e3c5a68fc28d2eb
Mateus Cruz <makteus@gmail.com>
2021-04-10 00:30:54 -0300

    Document have_attribute matcher

--- lib/rspec_jsonapi_serializer/matchers.rb
+++ lib/rspec_jsonapi_serializer/matchers.rb
@@ -4,6 +4,10 @@ require "rspec_jsonapi_serializer/matchers/have_attribute_matcher"
 
 module RSpecJSONAPISerializer
   module Matchers
+    # This allows us to assert attributes on a serializer, e.g.:
+    # expect(serializer).to have_attribute(:email)
+    # You can test custom attributes by using the submatcher "as".
+    # expect(serializer).to have_attribute(:email).as('john.doe@example.com')
     def have_attribute(expected)
       HaveAttributeMatcher.new(expected)
     end
