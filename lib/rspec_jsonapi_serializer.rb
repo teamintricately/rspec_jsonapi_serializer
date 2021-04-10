a4ed1cdb86724f51cfc08b9cdbf650f39d9dab84
Mateus Cruz <makteus@gmail.com>
2021-04-09 22:25:46 -0300

    Bundle gem

---
+++ lib/rspec_jsonapi_serializer.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rspec_jsonapi_serializer/version"
+
+module RSpecJSONAPISerializer
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/rspec_jsonapi_serializer_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe RSpecJSONAPISerializer do
+  it "has a version number" do
+    expect(RSpecJSONAPISerializer::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
